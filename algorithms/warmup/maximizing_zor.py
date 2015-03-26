16223a7dbcc4647698ae696f6ee8d03d6f157512
Neill Henning <neillhenning@gmail.com>
2015-03-26 15:51:01 +0200

    Add description for solution to maximizing xor

--- algorithms/warmup/maximizing_zor.py
+++ algorithms/warmup/maximizing_zor.py
@@ -1,5 +1,22 @@
 #!/bin/python
 
+"""
+Problem Statement:
+Given two integers, L and R, find the maximal value of A xor B, where A and B
+satisfy the following condition:
+L <= A <= B <= R
+
+Input Format:
+The input contains two lines; 
+L is present in the first line and R in the second line
+
+Constraints:
+1 <= L <= R <= 10**3
+
+Output Format:
+The maximal value as mentioned in the problem statement.
+"""
+
 # Complete the function below.
 def maxXor(l, r):
     # Set variables
